b060fb3430ea8aca19e04d3a2c2b119dca9b3951
benjaminthum <benjaminthum@aol.de>
2024-11-08 16:14:19 +0100

    test

--- k3s/create_lxc_k3s.sh
+++ k3s/create_lxc_k3s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source <(curl -s https://github.com/LordThum/bash/raw/refs/heads/main/k3s/lxc_preconfig/conf_lxc.sh)
-source <(curl -s https://github.com/LordThum/bash/raw/refs/heads/main/proxmox/create_lxc.sh)
+source /dev/stdin <<< "$(curl -s https://github.com/LordThum/bash/raw/refs/heads/main/k3s/lxc_preconfig/conf_lxc.sh)"
+source /dev/stdin <<< "$(curl -s https://github.com/LordThum/bash/raw/refs/heads/main/proxmox/create_lxc.sh)"
 
 start
 preconfig
